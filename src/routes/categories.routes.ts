002aefa92a1d51eb0c92a84d4321a6806f218019
wilembergson <wilembergson@yahoo.com>
2021-11-18 10:24:58 -0300

    Trabalhando com Arquivos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,14 @@
 import { Router,  } from "express"
-import { CategoryRepository } from "../modules/cars/repositories/implementations/CategoryRepository"
+import multer from "multer"
+
 import { createCategoryController } from "../modules/cars/useCases/createCategory"
+import { importCategoryController } from "../modules/cars/useCases/importCategory"
 import { listCategoryController } from "../modules/cars/useCases/listCategory"
 
 const categoriesRoutes = Router()
-const categoryRepository = CategoryRepository.getInstance()
-
+const upload = multer({
+    dest: "./temp"
+})
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response)
@@ -13,6 +16,10 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
     return listCategoryController.handle(request, response)
+})
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    return importCategoryController.handle(request, response)
 })
 
 export { categoriesRoutes }
