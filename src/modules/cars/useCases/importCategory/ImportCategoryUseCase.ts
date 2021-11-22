9b3f33909b7272162534f7ef921420232acbe6f2
wilembergson <wilembergson@yahoo.com>
2021-11-22 10:05:04 -0300

    Fim upload

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,16 +1,57 @@
 import fs from "fs"
 import { parse } from "csv-parse"
+import { ICategoryRepository } from "../../repositories/ICategoryRepository"
+
+interface IImportCategory{
+    name: string;
+    description: string;
+}
 
 class ImportCategoryUseCase{
-    execute(file: Express.Multer.File): void{
-        const stream = fs.createReadStream(file.path)
+    constructor(private categoryRepository: ICategoryRepository){}
 
-        const parseFile = parse()
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]>{
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path)
+            const categories: IImportCategory[] = []
 
-        stream.pipe(parseFile)
+            const parseFile = parse()
 
-        parseFile.on("data", async (line) => {
-            console.log(line)
+            stream.pipe(parseFile)
+
+            parseFile.on("data", async (line) => {
+                const [name, description] = line
+
+                categories.push({
+                    name,
+                    description
+                })
+            })
+            .on("end", ()=>{
+                resolve(categories)
+            })
+            .on("error", (err)=>{
+                reject(err)
+            })
+
+            return categories
+        })
+    }
+
+    async execute(file: Express.Multer.File): Promise<void>{
+        const categories = await this.loadCategories(file)
+        
+        categories.map(async (category) => {
+            const { name, description } = category
+
+            const existCategory = this.categoryRepository.findByName(name)
+
+            if(!existCategory){
+                this.categoryRepository.create({
+                    name,
+                    description
+                })
+            }
         })
     }
 }
